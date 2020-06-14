be773083e735336b691625eee79844cb5fe93ccd
Kaspar Märtens <kaspar.martens@gmail.com>
2020-06-15 00:06:08 +0100

    set up installable package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='NeuralDecomposition',
+      version='0.1',
+      description='Implementation of Neural Decomposition',
+      author='Kaspar Märtens',
+      author_email='kaspar.martens@spc.ox.ac.uk',
+      url='https://github.com/kasparmartens/NeuralDecomposition',
+      license='MIT',
+      packages=['ND'])
